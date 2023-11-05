da85f66249e9672957ca9cc3bc4ebb6ddaf47f00
doswald32 <dan.oswald32@gmail.com>
2023-11-04 22:45:08 -0600

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+    }
+    return newArray;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
